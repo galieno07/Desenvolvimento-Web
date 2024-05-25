7bd6e0b12a3b1bfae65b2820b201049b361673a3
galieno07 <galienofrancisco14@gmail.com>
2024-05-24 21:37:10 -0300

    Bom dia ADM

---
+++ LISTAS/Lista 5/js/ex05.js
@@ -0,0 +1,7 @@
+var nome = "Julio"
+var idade = 31
+var cargo = "Manutenção de Gerador"
+var Rua = " Rua Tiro ao Pombo"
+
+console.log(`\n Seu nome é ${nome} \n Você tem ${idade} anos \n Seu emprego atual e : ${cargo} \n E mora no endereço : ${Rua}`)
+
